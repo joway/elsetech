1f90619805f57272fd5f00ad7bebe6fed24ddd10
Joway <joway.w@gmail.com>
2026-06-19 16:03:58 +0800

    refactor analytics.js

--- analytics.js
+++ analytics.js
@@ -1,23 +1,30 @@
 window.dataLayer = window.dataLayer || [];
-// get domain
-var domain = window.location.hostname;
-// get userId
-var userId = window.localStorage.getItem("userId");
 
 function gtag() {
   dataLayer.push(arguments);
 }
 
-function googleAnalytics() {
-  gtag("js", new Date());
-  gtag("config", "G-SB31TR607X");
-  var event_action = "login";
+// get domain
+var domain = window.location.hostname;
+
+// get userId (returning user -> "login", new user -> "register")
+var userId;
+var event_action = "login";
+try {
+  userId = window.localStorage.getItem("userId");
   if (!userId) {
-    userId = Math.floor(Math.random() * 1000000000000);
+    userId = crypto.randomUUID();
     window.localStorage.setItem("userId", userId);
     event_action = "register";
   }
-  gtag("set", { user_id: userId });
+} catch (e) {
+  // storage unavailable (private mode / disabled) -> treat as anonymous session
+  userId = undefined;
+}
+
+function googleAnalytics() {
+  gtag("js", new Date());
+  gtag("config", "G-SB31TR607X", { user_id: userId });
   gtag("event", event_action, {
     event_category: "user",
     event_label: domain,
